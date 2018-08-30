b0a68843ab0c57555ae9927ddc122e273a345d9e
LamarC <dviann89@gmail.com>
2018-08-30 18:29:02 -0500

    Winnig ans losing on the screan

--- JS/logic.js
+++ JS/logic.js
@@ -1,35 +1,31 @@
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-console.log(options)
-console.log(userGuess)
+console.log(options);
 
 var wins = 0;
 var losses = 0;
 var attempsLeft = 0;
 
-var pcChoice = optionst[Math.floor(Math.random() * options.length)];
+var pcChoice = options[Math.floor(Math.random() * options.length)];
 var userGuess = [];
 
 document.onkeyup = function(event) {
   var userGuess = event.key;
- 
+ //event.keycode figure this one out 
+ //keyboard event
 }
 
-if {
-    (userGuess === pcChoice) {
+if (userGuess === pcChoice) {
         wins++;
     }
 
-else if {
-    (userGuess !== pcChoice) {
+else if 
+(userGuess !== pcChoice) {
         attempsLeft --;
     }
 
 else {
-    (attempsLeft = ) {
-        losses ++;
-    }
+    attempsLeft = losses ++;
 }
     document.getElementById("wins").innerHTML = "Wins: " + wins;
     document.getElementById("losses").innerHTML = "losses: " + losses;
     document.getElementById("attempsLeft").innerHTML = "Guesses left: " + guesses;
-}
